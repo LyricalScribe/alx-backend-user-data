7c042529d671b1f855f57a9e4cf20cf9a72751e1
Thulisile Ngomane <ThuleeN@outlook.com>
2023-08-04 03:08:10 +0000

    added fields constructor argument

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Process of Regex-ing"""
 import re
+import logging
 from typing import List
 
 
@@ -15,3 +16,27 @@ def filter_datum(
         redaction,
         message
     )
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, field: tuple):
+        """init methode"""
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format method to filter values in incoming log records
+        """
+        record.msg = filter_datum(
+            self.fields,
+            self.REDACTION,
+            record.msg,
+            self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
